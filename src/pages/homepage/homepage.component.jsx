c6224456e93cff94098207ea3bacab3fe4ecb394
rishabhbaluja <rishabhbaluja@gmail.com>
2020-07-04 08:12:20 +0530

    Divided homepage into smaller components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./homepage.styles.scss";
+import "../../components/directory/directory.component";
+import Directory from "../../components/directory/directory.component";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
